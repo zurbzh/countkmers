c21c704ed9909922a92e3186e4283230ee0c0fca
zurbzh <zurbzh@ITadmins-MacBook-Pro.local>
2018-01-30 11:55:06 +0200

    Refine

--- src/main/java/analyse.scala
+++ src/main/java/analyse.scala
@@ -7,20 +7,27 @@ import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 
 
 class analyser (db_file:String, delimiter:String) {
+
+
   val df = db_creation.receive(db_file, delimiter)
   df.registerTempTable("data")
 
   val sequence = df.select("sequence")
+  val sequences_as_list =  sequence.rdd.map(r => r.getString(0)).collect()
+
+
   val kmer_length = 3
   val letters = "ACGT"
   def comb(s:String)=(s * s.length).combinations(kmer_length)
-
   val kmers = comb(letters).flatMap(_.toString.permutations.toList).toList
 
- val kmer_list = ListBuffer[List[(String, Int)]]()
 
 
-  val sequences_as_list =  sequence.rdd.map(r => r.getString(0)).collect()
+
+  val kmer_list = ListBuffer[List[(String, Int)]]()
+
+
+
 
   def generate_kmers () {
 
@@ -36,8 +43,6 @@ class analyser (db_file:String, delimiter:String) {
       kmer_list ++= List(counted_kmers_by_sequence)
 
     }
-     kmer_list.foreach(println)
-
   }
 
 
